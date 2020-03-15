484e92f02f229f606ee73afe2a903cbdd130f88b
Lubomir I. Ivanov <lubomirivanov@vmware.com>
2020-03-15 21:51:55 +0200

    update verify.sh

--- k8s-repo-tools/verify.sh
+++ k8s-repo-tools/verify.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -o xtrace
 
+test -z "$(gofmt -d ./)" || exit 1
 go vet ./... || exit 1
 golint ./... || exit 1
-go test ./... || exit 1
+go test -count=1 ./... || exit 1
 staticcheck ./... || exit 1
